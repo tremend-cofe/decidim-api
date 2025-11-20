7ac73548f150f3c65c5914ef75ab0dddf0d95110
Alexandru Emil Lupu <contact@alecslupu.ro>
2025-11-21 01:40:57 +0200

    Add UpdateProposal GraphQL mutation with specs
    Co-authored-by: alecslupu <105683+alecslupu@users.noreply.github.com>
    
    # Conflicts:
    #	decidim-proposals/lib/decidim/proposals/api.rb

---
+++ decidim-proposals/spec/shared/update_proposal_mutation_examples.rb
@@ -0,0 +1,71 @@
+# frozen_string_literal: true
+
+shared_examples "update proposal mutation examples" do
+  context "when user is not authorized" do
+    let!(:user_type) { :user }
+
+    it "does not update the proposal" do
+      expect(response["update"]).to be_nil
+    end
+  end
+
+  context "when user is authorized" do
+    context "with valid attributes" do
+      it "updates the proposal" do
+        update = response["update"]
+        expect(update).to be_present
+        expect(update).to include(
+          {
+            "id" => model.id.to_s,
+            "title" => {
+              "translation" => new_title
+            },
+            "body" => {
+              "translation" => new_body
+            }
+          }
+        )
+      end
+
+      context "with address and coordinates" do
+        let(:address) { "Carrer de la Pau, 1, Barcelona" }
+        let(:latitude) { 41.3851 }
+        let(:longitude) { 2.1734 }
+        let(:variables) do
+          {
+            input: {
+              attributes: {
+                title: new_title,
+                body: new_body,
+                address:,
+                latitude:,
+                longitude:
+              }
+            }
+          }
+        end
+
+        it "updates the proposal with location data" do
+          update = response["update"]
+          expect(update).to be_present
+          expect(update).to include(
+            {
+              "id" => model.id.to_s,
+              "address" => address
+            }
+          )
+        end
+      end
+    end
+
+    context "with invalid attributes" do
+      let(:new_title) { "short" }
+      let(:new_body) { "x" }
+
+      it "returns an error" do
+        expect(response["update"]).to be_present
+        expect(response["update"]["message"]).to be_present
+      end
+    end
+  end
+end
