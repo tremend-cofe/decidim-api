9f8ec27b3a9b7024834b1cf2a7b2dd6f253ea556
Alexandru Emil Lupu <contact@alecslupu.ro>
2025-11-20 23:38:11 +0200

    Add debug permissions

--- decidim-api/lib/decidim/api/schema.rb
+++ decidim-api/lib/decidim/api/schema.rb
@@ -12,6 +12,16 @@ module Decidim
       max_complexity Decidim::Api.schema_max_complexity
 
       orphan_types(Api.orphan_types)
+
+      def self.unauthorized_object(error)
+        # Add a top-level error to the response instead of returning nil:
+        raise GraphQL::ExecutionError, "An object of type #{error.type.graphql_name} was hidden due to permissions"
+      end
+
+      def self.unauthorized_field(error)
+        # Add a top-level error to the response instead of returning nil:
+        raise GraphQL::ExecutionError, "The field #{error.field.graphql_name} on an object of type #{error.type.graphql_name} was hidden due to permissions"
+      end
     end
   end
 end
